7e0ad02f0c0afb6aeb9c3c0d12985005e4571e00
TsarBuig <70858574+tsarbuig@users.noreply.github.com>
2021-11-30 00:21:23 +0100

    Update LimitSwap.py

--- LimitSwap.py
+++ LimitSwap.py
@@ -941,7 +941,7 @@ def sell(amount, moonbag, inToken, outToken, gas, slippage, gaslimit, boost, fee
         if gas.lower() == 'boost':
             gas_check = client.eth.gasPrice
             gas_price = gas_check / 1000000000
-            gas = (gas_price * ((int(boost)*4)/100)) + gas_price
+            gas = (gas_price * ((int(boost))/100)) + gas_price
         else:
             gas = int(gas)
 
