c536adac347738e5d3e5aac3949e57eabfeb4734
TsarBuig <70858574+tsarbuig@users.noreply.github.com>
2022-01-09 00:04:31 +0100

    - Previous commit name was wrong, should have been :Migration of LiquidityCheck to MINIMUM_LIQUIDITY_IN_DOLLARS

--- LimitSwap.py
+++ LimitSwap.py
@@ -1667,7 +1667,7 @@ def auth():
     try:
         decode = decode_key()
     except Exception:
-        printt_err("There is a problem with your private key : please check if it's correct. Don't enter seed phrase!")
+        printt_err("There is a problem with your private key: please check if it's correct. Don't enter seed phrase!")
     
     wallet_address = Web3.toChecksumAddress(decode)
     balance = balanceContract.functions.balanceOf(wallet_address).call()
