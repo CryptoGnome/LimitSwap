7e4dd8f7e924984a338a86161a017a6a0441944c
TsarBuig <70858574+tsarbuig@users.noreply.github.com>
2022-01-17 15:06:36 +0100

    - Allowed % update in real time for ONE token for now

--- LimitSwap.py
+++ LimitSwap.py
@@ -59,6 +59,13 @@ repeated_message_quantity = 0
 # Global used for run()
 tokens_json_already_loaded = 0
 
+# Global used to save program_defined_values before update of tokens.json
+_COST_PER_TOKEN_saved = []
+_PREVIOUS_TOKEN_BALANCE_saved = []
+
+# Global used for WATCH_STABLE_PAIRS
+set_of_new_tokens = []
+
 # color styles
 class style():  # Class of different text colours - default is white
     BLACK = '\033[30m'
@@ -460,11 +467,10 @@ def load_tokens_file(tokens_path, load_message=True):
     
     printt_debug("ENTER load_tokens_file")
     
-    set_of_new_tokens = []
+    global set_of_new_tokens
 
     if load_message == True:
         print(timestamp(), "Loading tokens from", tokens_path)
-
 
     with open(tokens_path, ) as js_file:
         t = jsmin(js_file.read())
@@ -483,7 +489,6 @@ def load_tokens_file(tokens_path, load_message=True):
     
     default_false_settings = [
         'ENABLED',
-        'LIQUIDITYINNATIVETOKEN',
         'USECUSTOMBASEPAIR',
         'HASFEES',
         'RUGDOC_CHECK',
@@ -621,8 +626,8 @@ def load_tokens_file(tokens_path, load_message=True):
         
         for default_true in default_true_settings:
             if default_true not in token:
-                printt_v(default_true, "not found in configuration file in configuration for to token", token['SYMBOL'], "setting a default value of false")
-                token[default_true] = "false"
+                printt_v(default_true, "not found in configuration file in configuration for to token", token['SYMBOL'], "setting a default value of true")
+                token[default_true] = "true"
             else:
                 token[default_true] = token[default_true].lower()
         
@@ -684,27 +689,32 @@ def reload_tokens_file(tokens_path, load_message=True):
     
     printt_debug("ENTER reload_tokens_file")
     
+    global _COST_PER_TOKEN_saved
+    global _PREVIOUS_TOKEN_BALANCE_saved
+    global set_of_new_tokens
+
     if load_message == True:
         print(timestamp(), "Reloading tokens from", tokens_path)
 
-    s = open(tokens_path, )
-    tokens = json.load(s)
-    s.close()
+    with open(tokens_path, ) as js_file:
+        t = jsmin(js_file.read())
+    tokens = json.loads(t)
     
+    printt_debug("tokens1:", tokens)
+
     required_user_settings = [
         'ADDRESS',
         'BUYAMOUNTINBASE',
         'BUYPRICEINBASE',
         'SELLPRICEINBASE'
     ]
-    
+
     default_true_settings = [
         'LIQUIDITYINNATIVETOKEN'
     ]
-    
+
     default_false_settings = [
         'ENABLED',
-        'LIQUIDITYINNATIVETOKEN',
         'USECUSTOMBASEPAIR',
         'HASFEES',
         'RUGDOC_CHECK',
@@ -714,7 +724,7 @@ def reload_tokens_file(tokens_path, load_message=True):
         'WAIT_FOR_OPEN_TRADE',
         'WATCH_STABLES_PAIRS'
     ]
-    
+
     default_value_settings = {
         'SLIPPAGE': 49,
         'BUYAMOUNTINTOKEN': 0,
@@ -736,8 +746,7 @@ def reload_tokens_file(tokens_path, load_message=True):
         'BUYCOUNT': 0,
         '_STABLE_BASES': {}
     }
-
-    # how can i NOT update those  program_defined_values, and let the previous ones ??
+    
     program_defined_values = {
         '_LIQUIDITY_READY': False,
         '_LIQUIDITY_CHECKED': False,
@@ -751,7 +760,7 @@ def reload_tokens_file(tokens_path, load_message=True):
         '_SUCCESS_TRANSACTIONS': 0,
         '_REACHED_MAX_SUCCESS_TX': False,
         '_TOKEN_BALANCE': 0,
-        '_PREVIOUS_TOKEN_BALANCE': 0,
+        '_PREVIOUS_TOKEN_BALANCE': _PREVIOUS_TOKEN_BALANCE_saved,
         '_BASE_BALANCE': 0,
         '_BASE_PRICE': 0,
         '_BASE_USED_FOR_TX': 0,
@@ -760,8 +769,8 @@ def reload_tokens_file(tokens_path, load_message=True):
         '_QUOTE': 0,
         '_PREVIOUS_QUOTE': 0,
         '_ALL_TIME_HIGH': 0,
-        '_COST_PER_TOKEN': 0,
-        '_CALCULATED_SELLPRICEINBASE': 0,
+        '_COST_PER_TOKEN': _COST_PER_TOKEN_saved,
+        '_CALCULATED_SELLPRICEINBASE': 99999,
         '_CALCULATED_STOPLOSSPRICEINBASE': 0,
         '_ALL_TIME_LOW': 0,
         '_CONTRACT_DECIMALS': 0,
@@ -774,9 +783,9 @@ def reload_tokens_file(tokens_path, load_message=True):
         '_EXCHANGE_BASE_SYMBOL': settings['_EXCHANGE_BASE_SYMBOL'],
         '_PAIR_SYMBOL': ''
     }
-    
+
     for token in tokens:
-        
+    
         # Keys that must be set
         for required_key in required_user_settings:
             if required_key not in token:
@@ -784,38 +793,56 @@ def reload_tokens_file(tokens_path, load_message=True):
                 printt_err("Be careful, sometimes new parameter are added : please check default tokens.json file")
                 sleep(20)
                 exit(-1)
-
+    
         for default_false in default_false_settings:
             if default_false not in token:
                 printt_v(default_false, "not found in configuration file in configuration for to token", token['SYMBOL'], "setting a default value of false")
                 token[default_false] = "false"
             else:
                 token[default_false] = token[default_false].lower()
-
+    
         for default_true in default_true_settings:
             if default_true not in token:
-                printt_v(default_true, "not found in configuration file in configuration for to token", token['SYMBOL'], "setting a default value of false")
-                token[default_true] = "false"
+                printt_v(default_true, "not found in configuration file in configuration for to token", token['SYMBOL'], "setting a default value of true")
+                token[default_true] = "true"
             else:
                 token[default_true] = token[default_true].lower()
-
-        # Set program values only if they haven't been set already
-        if '_LIQUIDITY_READY' not in token:
-            for value in program_defined_values:
-                token[value] = program_defined_values[value]
-
+    
         for default_key in default_value_settings:
             if default_key not in token:
                 printt_v(default_key, "not found in configuration file in configuration for to token", token['SYMBOL'], "setting a value of", default_value_settings[default_key])
                 token[default_key] = default_value_settings[default_key]
             elif default_key == 'SELLAMOUNTINTOKENS':
                 default_value_settings[default_key] = default_value_settings[default_key].lower()
-
+    
+        # Set program values only if they haven't been set already
+        if '_LIQUIDITY_READY' not in token:
+            for value in program_defined_values:
+                token[value] = program_defined_values[value]
+    
+        for key in token:
+            if (isinstance(token[key], str)):
+                if re.search(r'^\d*\.\d+$', str(token[key])):
+                    token[key] = float(token[key])
+                elif re.search(r'^\d+$', token[key]):
+                    token[key] = int(token[key])
+    
+        if token['WATCH_STABLES_PAIRS'] == 'true' and token['USECUSTOMBASEPAIR'] == 'false':
+            for new_token_dict in build_extended_base_configuration(token):
+                set_of_new_tokens.append(new_token_dict)
+        elif token['WATCH_STABLES_PAIRS'] == 'true':
+            printt_warn("Ignoring WATCH_STABLES_PAIRS", "for", token['SYMBOL'], ": WATCH_STABLES_PAIRS = true and USECUSTOMBASEPAIR = true is unsupported.")
+    
         if token['USECUSTOMBASEPAIR'] == 'false':
             token['_PAIR_SYMBOL'] = token['SYMBOL'] + '/' + token['_EXCHANGE_BASE_SYMBOL']
         else:
             token['_PAIR_SYMBOL'] = token['SYMBOL'] + '/' + token['BASESYMBOL']
 
+    # Add any tokens generated by "WATCH_STABLES_PAIRS" to the tokens list.
+    for token_dict in set_of_new_tokens:
+        tokens.append(token_dict)
+
+    printt_debug("EXIT reload_tokens_file")
     return tokens
 
 
@@ -2392,7 +2419,7 @@ def build_sell_conditions(token_dict, condition):
     if re.search('^(\d+\.){0,1}\d+%$', str(sell)):
         sell = sell.replace("%","")
         if condition == 'before_buy':
-            printt_err("Be careful, updating sellprice with % in real-time does NOT work for the moment. Bot will set SELLPRICE = 99999")
+            printt_err("Be careful, updating sellprice with % in real-time WORKS ONLY FOR ONE TOKEN for the moment)
             printt_err("----------------------------------------------------------------------------------------------------------------------------------")
             printt_err("   --> do NOT change your tokens.json or close the bot after BUY order is made, or your calculated SELLPRICE will be lost!")
             printt_err("----------------------------------------------------------------------------------------------------------------------------------")
@@ -2404,7 +2431,7 @@ def build_sell_conditions(token_dict, condition):
             printt_info(token_dict['SYMBOL'], " cost per token was: ", token_dict['_COST_PER_TOKEN'])
             printt_info("--> SELLPRICEINBASE = ", token_dict['SELLPRICEINBASE'],"*", token_dict['_COST_PER_TOKEN'], "= ", token_dict['_CALCULATED_SELLPRICEINBASE'])
             printt_info("")
-            printt_err("DO NOT CHANGE TOKENS.JSON OR CLOSE THE BOT OR THIS INFORMATION WILL BE LOST")
+            printt_err("DO NOT CLOSE THE BOT OR THIS INFORMATION WILL BE LOST")
             printt_info("---------------------------------------------------------------------------")
     # Otherwise, don't adjust the sell price in base
     else:
@@ -2414,7 +2441,7 @@ def build_sell_conditions(token_dict, condition):
     if re.search('^(\d+\.){0,1}\d+%$', str(stop)):
         stop = stop.replace("%","")
         if condition == 'before_buy':
-            printt_err("Be careful, updating sellprice with % in real-time does NOT work for the moment. Bot will set STOPLOSSPRICE = 0")
+            printt_err("Be careful, updating stoplossprice with % in real-time WORKS ONLY FOR ONE TOKEN for the moment")
             printt_info("Since you have put a % in SELLPRICE, and bot did not buy yet, we will set STOPLOSSPRICE = 0.")
             token_dict['_CALCULATED_STOPLOSSPRICEINBASE'] = 0
         else:
@@ -4394,6 +4421,9 @@ def benchmark():
     
 def run():
     global tokens_json_already_loaded
+    global _COST_PER_TOKEN_saved
+    global _PREVIOUS_TOKEN_BALANCE_saved
+    
     tokens_json_already_loaded = tokens_json_already_loaded + 1
     try:
         
@@ -4403,7 +4433,7 @@ def run():
         if tokens_json_already_loaded > 1:
             printt_debug("try to launch reload_tokens_file")
             #TODO : we should be using reload_tokens_file, but it does not work for now
-            tokens = load_tokens_file(command_line_args.tokens, True)
+            tokens = reload_tokens_file(command_line_args.tokens, True)
 
         # Display the number of token pairs we're attempting to trade
         token_list_report(tokens)
@@ -4830,6 +4860,9 @@ def run():
         printt_debug("tokens_json_already_loaded: ", tokens_json_already_loaded)
         if tokens_json_already_loaded > 0:
             printt_debug("Debug 4841 - reload_tokens_file condition")
+            _COST_PER_TOKEN_saved = token['_COST_PER_TOKEN']
+            _PREVIOUS_TOKEN_BALANCE_saved = token['_PREVIOUS_TOKEN_BALANCE']
+            printt_debug("4838 _COST_PER_TOKEN_saved:", _COST_PER_TOKEN_saved)
             reload_bot_settings(bot_settings)
             sleep(0.01)
             logging.exception(ee)
