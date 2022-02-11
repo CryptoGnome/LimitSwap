88d322c434bb9aebffb5af5de455234d9ae6f51b
TsarBuig <70858574+tsarbuig@users.noreply.github.com>
2022-02-11 15:35:59 +0100

    v4.2.2.1

--- LimitSwap.py
+++ LimitSwap.py
@@ -1118,7 +1118,7 @@ printt("************************************************************************
 
 # Check for version
 #
-version = '4.2.2'
+version = '4.2.2.1'
 printt("YOUR BOT IS CURRENTLY RUNNING VERSION ", version, write_to_log=True)
 check_release()
 
