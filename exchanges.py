e76c7fa34fa3a0a615e03ff3f723ef8788671559
TsarBuig <70858574+tsarbuig@users.noreply.github.com>
2022-04-06 16:29:48 +0200

    Obfuscate code

--- exchanges.py
+++ exchanges.py
@@ -1,11 +1,16 @@
 # Sorry, LimitSwap source code is not available anymore
-
+# Please download the .py or the .exe version in the "Assets" menu
+#
 # There was too many scammers on the channel telling people they could disable LIMIT token check
 # --> Sadly, I am forced to protect my software and obfuscate the code.
 # --> the source code won't be available anymore, I'm sorry guys it's sad 👎
-
+print("")
+print("")
 print("Sorry, LimitSwap source code is not available anymore")
+print("Please download the .py or the .exe version in the Assets menu")
 print("")
 print("There was too many scammers on the channel telling people they could disable LIMIT token check")
 print("--> Sadly, I am forced to protect my software and obfuscate the code.")
 print("--> the source code won't be available anymore, I'm sorry guys it's sad 👎")
+print("")
+print("")
